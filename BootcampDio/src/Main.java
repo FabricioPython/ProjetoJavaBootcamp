ae4dfc3d3f039907a31f91e2a9acfb7f568e1aef
FabricioPython <fabricio.mh@outlook.com>
2023-02-14 19:05:43 -0300

    testes classes

--- BootcampDio/src/Main.java
+++ BootcampDio/src/Main.java
@@ -1,9 +1,41 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//testando
+		Curso nomeCurso = new Curso();
+		
+		Mentoria mentoriaJava = new Mentoria();
+		
+		System.out.println(nomeCurso);
+		
+		nomeCurso.setTitulo("java básico");
+		nomeCurso.setDescricao("Elementos fundamentais da Sintaxe java e principais pacotes");
+		nomeCurso.setCargaHoraria(200);
+		
+		
+		System.out.println(nomeCurso);
+		
+		System.out.println("\n\n\n");
+		
+		System.out.println(mentoriaJava);
+		
+		mentoriaJava.setData(LocalDate.now());
+		mentoriaJava.setDescricao("Estruturas de repetição e iterator.");
+		mentoriaJava.setTitulo("Lopps e interações");
+		System.out.println(mentoriaJava);
+		
+		
+		
 
+
+		
 	}
 
 }
